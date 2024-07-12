1bf940d8851ae96b30e9782c6a0fd2730797392f
carloguenzl <carlo@guenzl.eu>
2024-07-12 23:24:43 +0200

    new measurements

--- get_spe_data.py
+++ get_spe_data.py
@@ -10,7 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-path = "C:/Users/carlo/OneDrive/Fortgeschrittenenpraktikum/FEL/Erste Messungen/sr90 mit Cu.Spe"
+path = "C:/Users/carlo/OneDrive/Fortgeschrittenenpraktikum/FEL/Erste Messungen/Kalibrierung.Spe"
 
 
 def get_spedata(path):
@@ -19,9 +19,15 @@ def get_spedata(path):
     content = Files.readlines()
     Files.close()
     borders = (int(content[11].split(" ")[0]), int(content[11].split(" ")[1].replace("\n","")))
-    data = [int(content[i]) for i in range(borders[0]+12,borders[1]+12)]
+    data = np.array([int(content[i]) for i in range(borders[0]+12,borders[1]+12)])
     return data
 
 result = get_spedata(path)
 
-plt.plot(result)
+#%%
+plt.figure(dpi=150)
+plt.plot(result,linewidth=.5,c=(204/255,0,0))
+plt.grid()
+plt.xlabel("Energy")
+plt.ylabel("count")
+#plt.savefig("C:/Users/carlo/Desktop/Dateidatei/6. SEMESTER/F-Praktikum/FEL/pics/spectran_rauschen.png",dpi=200)
